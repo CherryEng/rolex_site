197943f6daea969793f04a749d6cd53dcf64c934
CherryEng <ted08z@outlook.jp>
2026-02-27 21:58:42 +0900

    背景画像パス指定変更

--- js/index/bgImages.js
+++ js/index/bgImages.js
@@ -6,6 +6,6 @@ export const initBgImages = () => {
 	const items = qsa('.details_link');
 	items.forEach((item, index) => {
 		const num = String(index + 1).padStart(2, '0');
-		item.style.backgroundImage = `url(../images/top/bg_watches/watch_${num}.jpg)`;
+		item.style.backgroundImage = `url(images/top/bg_watches/watch_${num}.jpg)`;
 	});
 };
